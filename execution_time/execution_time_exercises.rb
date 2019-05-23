a99f604e3042097cc891c3b38ea4688bb596afb0
ken8waru1 <ken8waru1@gmail.com>
2019-05-23 16:19:22 -0700

    Finish the longest_contiguous sum

--- execution_time/execution_time_exercises.rb
+++ execution_time/execution_time_exercises.rb
@@ -17,4 +17,46 @@ def my_min2(list)
 
   min
 end
- 
+
+# def largest_contiguous_sum(list)
+#   sub_arrays = []
+#   list.each_with_index do |el1, i|
+#     (i...list.length).each_with_index do |el2, j|
+#       sub_arrays << list[i..j] if j >= i
+#     end
+#   end
+#   sub_arrays
+# end
+
+def lcs(list)
+  sub_arrays = []
+  list.each_with_index do |el1, idx1|
+    sub_arrays << [el1]
+    list.each_with_index do |el2, idx2|
+      sub_arrays << sub_arrays[-1] + [el2] if idx2 > idx1
+    end
+  end
+  sub_arrays
+  sub_arrays.map {|sub| sub.sum }.max
+
+end
+require 'byebug'
+def lcs_better(list)
+  cs = 0
+  ls = list.first
+
+  list.each do |ele|
+    # debugger
+    if ele > cs + ele
+      cs = ele
+    else
+      cs += ele
+    end
+    ls = cs if cs > ls
+  end
+  
+  ls
+end
+#[2, 3, -6, 7, -6, 7]
+
+# list = [-5, -1, -3]
